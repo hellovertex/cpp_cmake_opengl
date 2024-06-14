47819412b4b668b977badfc42ea502bb410db8b2
sascha <hellovertex@outlook.com>
2024-06-14 16:20:40 +0200

    wip - add missing tests_main.cc

---
+++ tests/tests_main.cc
@@ -0,0 +1,10 @@
+////
+//// Created by sascha on 14.06.24.
+////
+#include <gtest/gtest.h>
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
