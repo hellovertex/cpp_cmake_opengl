c46e9e5d8806e97c75177311fdb1641d532baf98
sascha <hellovertex@outlook.com>
2024-06-14 16:18:56 +0200

    wip - add missing files

---
+++ code/source_files/event_dispatcher.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include <functional>
+#include <map>
+#include <string>
+#include <vector>
+
+// todo: create EventDispatcher object inside namespace EventDispatcher
+namespace EventDispatcher {
+//    void hello() {
+//        std::cout << "Hello From Dispatcher";
+//    }
+    class EventDispatcher {
+
+    };
+
+}
--- tests/test_dispatcher.cc
+++ tests/test_dispatcher.cc
@@ -1,6 +1,49 @@
 #include <gtest/gtest.h>
 #include "event_dispatcher.h"
+//
+//class EventDispatcherTest : public ::testing::Test {
+//protected:
+//    EventDispatcher::EventDispatcher dispatcher;
+//
+//    static void testCallback(void* event) {
+//        ++callbackCount;
+//    }
+//
+//    static int callbackCount;
+//
+//    void SetUp() override {
+//        callbackCount = 0;
+//    }
+//};
+//
+//int EventDispatcherTest::callbackCount = 0;
+//
+//TEST_F(EventDispatcherTest, AddEventListener) {
+//    dispatcher.addEventListener("testEvent", testCallback);
+//    ASSERT_TRUE(dispatcher.hasEventListener("testEvent", testCallback));
+//}
+//
+//TEST_F(EventDispatcherTest, RemoveEventListener) {
+//    dispatcher.addEventListener("testEvent", testCallback);
+//    dispatcher.removeEventListener("testEvent", testCallback);
+//    ASSERT_FALSE(dispatcher.hasEventListener("testEvent", testCallback));
+//}
+//
+//TEST_F(EventDispatcherTest, DispatchEvent) {
+//    dispatcher.addEventListener("testEvent", testCallback);
+//    dispatcher.dispatchEvent("testEvent");
+//    ASSERT_EQ(callbackCount, 1);
+//}
+//
+//TEST_F(EventDispatcherTest, NoCallbackCalledIfListenerRemoved) {
+//    dispatcher.addEventListener("testEvent", testCallback);
+//    dispatcher.removeEventListener("testEvent", testCallback);
+//    dispatcher.dispatchEvent("testEvent");
+//    ASSERT_EQ(callbackCount, 0);
+//}
 
+// todo: create Test class that creates event dispatcher instance and calls
+//  instance methods using namespace notation
 TEST(SampleTest, AssertionTrue) {
     EventDispatcher::hello();
 
