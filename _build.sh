6f41d367e45b02a957ed289a69948ee0ac361edf
hellovertex <hellovertex@outlook.com>
2024-06-22 20:24:22 +0200

    upd sync

--- _build.sh
+++ _build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# on windows: cmake --build out/build_windows --config Debug
+#  the above should work on Linux too (replacing build_windows with build_linux)
 cd out/build_linux && make && cd ../..
--- _configure.sh
+++ _configure.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # cmake -S . -DCMAKE_TOOLCHAIN_FILE=./toolchains/Toolchain-x86_64-w64-mingw32.cmake -B out/build_windows
+# on windows: cmake -S . -B out/build_windows
 cmake -S . -B out/build_linux
